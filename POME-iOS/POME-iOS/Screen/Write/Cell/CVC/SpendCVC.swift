ec6b8e3a3e46b87586e2d673620ebf41c19e4e83
juhyeon <wngus4296@swu.ac.kr>
2022-07-08 01:47:11 +0900

    [FEAT] 씀씀이 셀 구현

---
+++ POME-iOS/POME-iOS/Screen/Write/Cell/CVC/SpendCVC.swift
@@ -0,0 +1,17 @@
+//
+//  SpendCVC.swift
+//  POME-iOS
+//
+//  Created by Juhyeon Byun on 2022/07/08.
+//
+
+import UIKit
+
+class SpendCVC: UICollectionViewCell {
+
+    // MARK: Life Cycle
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
