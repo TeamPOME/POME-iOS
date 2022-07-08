696fc01698955d9e40d720563b30c26da677a4ab
juhyeon <wngus4296@swu.ac.kr>
2022-07-09 01:57:46 +0900

    [FEAT] WriteSB 구성 및 WriteVC에 CV 연결 및 구성 (#8)

--- POME-iOS/POME-iOS/Screen/Write/VC/WriteVC.swift
+++ POME-iOS/POME-iOS/Screen/Write/VC/WriteVC.swift
@@ -7,23 +7,85 @@
 
 import UIKit
 
-class WriteVC: UIViewController {
+class WriteVC: BaseVC {
 
+    // MARK: IBOutlet
+    @IBOutlet weak var writeHomeNaviBar: PomeNaviBar!
+    @IBOutlet weak var goalCV: UICollectionView!
+    @IBOutlet weak var writeMainCV: UICollectionView!
+    
+    // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setMainCV()
+        configureUI()
+    }
+}
 
-        // Do any additional setup after loading the view.
+// MARK: - UI
+extension WriteVC {
+    private func configureUI() {
+        writeHomeNaviBar.setNaviStyle(state: .greyWithRightBtn)
+    }
+}
+
+// MARK: - UICollectionViewDelegate
+extension WriteVC: UICollectionViewDelegate {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 3
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+extension WriteVC: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        switch section {
+        case 0, 1:
+            return 1
+        default:
+            return 10
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let goalCardCVC = writeMainCV.dequeueReusableCell(withReuseIdentifier: GoalCardCVC.className, for: indexPath) as? GoalCardCVC else { return UICollectionViewCell() }
+        guard let feelingCardCVC = writeMainCV.dequeueReusableCell(withReuseIdentifier: FeelingCardCVC.className, for: indexPath) as? FeelingCardCVC else {
+                    return UICollectionViewCell() }
+        guard let spendCVC = writeMainCV.dequeueReusableCell(withReuseIdentifier: SpendCVC.className, for: indexPath) as? SpendCVC else { return UICollectionViewCell() }
+        
+        switch indexPath.section {
+        case 0:
+            return goalCardCVC
+        case 1:
+            return feelingCardCVC
+        default:
+            return spendCVC
+        }
     }
-    */
+}
 
+// MARK: - UICollectionViewDelegateFlowLayout
+extension WriteVC: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        switch indexPath.section {
+        case 0:
+            return CGSize(width: 343.adjusted, height: 157)
+        case 1:
+            return CGSize(width: 343.adjusted, height: 118)
+        default:
+            return CGSize(width: 166, height: 188)
+        }
+    }
+}
+
+// MARK: - Custom Methods
+extension WriteVC {
+    private func setMainCV() {
+        writeMainCV.delegate = self
+        writeMainCV.dataSource = self
+        FeelingCardCVC.register(target: writeMainCV)
+        GoalCardCVC.register(target: writeMainCV)
+        SpendCVC.register(target: writeMainCV)
+    }
 }
