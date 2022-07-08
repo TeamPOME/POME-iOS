e84cbd130242638f78691130a27ffdf4a49033bf
juhyeon <wngus4296@swu.ac.kr>
2022-07-09 01:30:13 +0900

    [FIX] conflict 해결 (#8)

---
+++ POME-iOS/POME-iOS/Screen/Write/Cell/CVC/SpendCVC.swift
@@ -0,0 +1,23 @@
+//
+//  SpendCVC.swift
+//  POME-iOS
+//
+//  Created by Juhyeon Byun on 2022/07/08.
+//
+
+import UIKit
+
+class SpendCVC: BaseCVC {
+
+    // MARK: IBOutlet
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var leftEmojiImageView: UIImageView!
+    @IBOutlet weak var rightEmojiImageView: UIImageView!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var contentLabel: UILabel!
+    
+    // MARK: Life Cycle
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
