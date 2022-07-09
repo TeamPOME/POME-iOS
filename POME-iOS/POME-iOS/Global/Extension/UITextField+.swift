db469c966b8c8f57951d016957a2a2c705e8f5cc
eunjuchoi <eunjuzzoo@naver.com>
2022-07-09 16:15:18 +0900

    [ADD] UITextField+ 함수 추가 (#10)

--- POME-iOS/POME-iOS/Global/Extension/UITextField+.swift
+++ POME-iOS/POME-iOS/Global/Extension/UITextField+.swift
@@ -37,4 +37,36 @@ extension UITextField {
         attributedStr.addAttribute(NSAttributedString.Key.kern, value: spacing, range: NSMakeRange(0, attributedStr.length))
         self.attributedText = attributedStr
      }
+    
+    /// placeholder 색상 설정 메서드
+    func setPlaceholderColor(_ placeholderColor: UIColor) {
+        attributedPlaceholder = NSAttributedString(
+            string: placeholder ?? "",
+            attributes: [
+                .foregroundColor: placeholderColor,
+                .font: font
+            ].compactMapValues { $0 }
+        )
+    }
+    
+    /// clear 버튼 커스텀 메서드
+    func configureClearBtnWithImage(image : UIImage) {
+        let clearBtn = UIButton(type: .custom)
+        clearBtn.setImage(image, for: .normal)
+        clearBtn.frame = CGRect(x: 0, y: 0, width: 20, height: 20)
+        clearBtn.contentMode = .scaleAspectFit
+        clearBtn.addTarget(self, action: #selector(UITextField.clear(sender:) ), for: .touchUpInside)
+        
+        let rightView = UIView(frame: CGRect(x: 0, y: 0, width: 36, height: 20))
+        rightView.addSubview(clearBtn)
+        
+        self.rightView = rightView
+        self.rightViewMode = .always
+    }
+
+    /// 텍스트필드 텍스트 clear 메서드
+    @objc func clear(sender : AnyObject) {
+        self.text = ""
+        sendActions(for: .editingChanged)
+    }
 }
