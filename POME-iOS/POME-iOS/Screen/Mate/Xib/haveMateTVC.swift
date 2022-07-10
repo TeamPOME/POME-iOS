fe961f7831b8d6c0fc29fa9a9faf3f8a6dc93f37
yujindonut <hyujin0825@gmail.com>
2022-07-10 14:03:08 +0900

    [FEAT] haveMateTVC UI 생성

---
+++ POME-iOS/POME-iOS/Screen/Mate/Xib/haveMateTVC.swift
@@ -0,0 +1,41 @@
+//
+//  haveMateTVC.swift
+//  POME-iOS
+//
+//  Created by 한유진 on 2022/07/10.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class haveMateTVC: CodeBaseTVC {
+    
+    // MARK: Properties
+    @IBOutlet weak var BiggestContainerView: UIView!
+    @IBOutlet weak var ContainerView: UIView!
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var spentMoneyLabel: UILabel!
+    @IBOutlet weak var contentLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var sadEmojiContainerView: UIView!
+    @IBOutlet weak var sadEmojiBtn: UIButton!
+    @IBOutlet weak var smileEmojiContainerView: UIView!
+    @IBOutlet weak var smileEmojiBtn: UIButton!
+    @IBOutlet weak var goalLabelContainerView: UIView!
+    @IBOutlet weak var goalLabel: UILabel!
+    @IBOutlet weak var firstEmojiContainerBtn: UIButton!
+    @IBOutlet weak var secondEmojiContainerBtn: UIButton!
+    @IBOutlet weak var thirdEmojiContainerBtn: UIButton!
+    @IBOutlet weak var countMateLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
+
+// MARK: - UI
+extension haveMateTVC {
+    
+}
