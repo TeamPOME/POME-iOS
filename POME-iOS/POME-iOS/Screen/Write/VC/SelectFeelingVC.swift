b3063154f110fb85aa3ce390f01258643c316c48
juhyeon <wngus4296@swu.ac.kr>
2022-07-11 16:42:21 +0900

    [FEAT] 버튼 선택에 따른 이미지, 레이블 변경 구현 (#25)

--- POME-iOS/POME-iOS/Screen/Write/VC/SelectFeelingVC.swift
+++ POME-iOS/POME-iOS/Screen/Write/VC/SelectFeelingVC.swift
@@ -8,10 +8,16 @@
 import UIKit
 
 class SelectFeelingVC: BaseVC {
-
+    
     // MARK: IBOutlet
     @IBOutlet weak var naviBar: PomeNaviBar!
     @IBOutlet weak var saveEmojiBtn: PomeBtn!
+    @IBOutlet weak var happyBtn: UIButton!
+    @IBOutlet weak var happyLabel: UILabel!
+    @IBOutlet weak var dontKnowBtn: UIButton!
+    @IBOutlet weak var dontKnowLabel: UILabel!
+    @IBOutlet weak var regretBtn: UIButton!
+    @IBOutlet weak var regretLabel: UILabel!
     
     // MARK: Life Cycle
     override func viewDidLoad() {
@@ -21,6 +27,22 @@ class SelectFeelingVC: BaseVC {
     }
     
     // MARK: IBAction
+    @IBAction func tapEmojiBtn(_ sender: UIButton) {
+        sender.isSelected.toggle()
+        
+        /// 선택된 버튼 제외한 버튼 select 상태를 false로 바꿈
+        [happyBtn, dontKnowBtn, regretBtn].forEach { btn in
+            if sender != btn {
+                btn?.isSelected = false
+            }
+        }
+        
+        configureHappyUI()
+        configureDontKnowUI()
+        configureRegretUI()
+        
+        saveEmojiBtn.isDisabled = !(happyBtn.isSelected || dontKnowBtn.isSelected || regretBtn.isSelected)
+    }
     
 }
 
@@ -34,5 +56,23 @@ extension SelectFeelingVC {
     private func configureUI() {
         saveEmojiBtn.setTitle("남겼어요", for: .normal)
         saveEmojiBtn.isDisabled = true
+    }
+    
+    /// 행복해요 버튼 선택에 따른 이미지와 레이블 색 설정 메서드
+    private func configureHappyUI() {
+        happyBtn.setImgByName(name: "btnEmojiHappyPink110Nor", selectedName: "btnEmojiHappyPink110Sel")
+        happyLabel.textColor = happyBtn.isSelected ? .sub : .grey_7
+    }
+    
+    /// 모르겠어요 버튼 선택에 따른 이미지와 레이블 색 설정 메서드
+    private func configureDontKnowUI() {
+        dontKnowBtn.setImgByName(name: "btnEmojiWhatPink110Nor", selectedName: "btnEmojiWhatPink110Sel")
+        dontKnowLabel.textColor = dontKnowBtn.isSelected ? .sub : .grey_7
+    }
+    
+    /// 후회해요 버튼 선택에 따른 이미지와 레이블 색 설정 메서드
+    private func configureRegretUI() {
+        regretBtn.setImgByName(name: "btnEmojiSadPink110Nor", selectedName: "btnEmojiSadPink110Sel")
+        regretLabel.textColor = regretBtn.isSelected ? .sub : .grey_7
     }
 }
