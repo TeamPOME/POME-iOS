6bfc51a56675f58884b2d30468f4b87d41aa0e3c
juhyeon <wngus4296@swu.ac.kr>
2022-07-15 03:13:59 +0900

    [FEAT] 카드 탭 제스처 처리 (#48)

--- POME-iOS/POME-iOS/Screen/Write/Cell/CVC/FeelingCardCVC.swift
+++ POME-iOS/POME-iOS/Screen/Write/Cell/CVC/FeelingCardCVC.swift
@@ -11,17 +11,33 @@ class FeelingCardCVC: BaseCVC {
     
     // MARK: IBOutlet
     @IBOutlet weak var remindNumLabel: UILabel!
+    @IBOutlet weak var cardView: UIView!
     
     // MARK: Life Cycle
     override func awakeFromNib() {
         super.awakeFromNib()
+        setTapEvent()
     }
 }
 
 // MARK: - Custom Methods
 extension FeelingCardCVC {
     
+    private func setTapEvent() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tapCard))
+        cardView.addGestureRecognizer(tapGesture)
+    }
+    
     func setData(num: Int) {
         remindNumLabel.text = "다시 돌아볼 씀씀이가 \(num)건 있어요"
+    }
+}
+
+// MARK: - @objc
+extension FeelingCardCVC {
+    
+    /// 카드를 탭 한 경우 처리
+    @objc func tapCard() {
+        
     }
 }
