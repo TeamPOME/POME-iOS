faa06d7efae11d1c1bb45888104ed4636182e1d5
juhyeon <wngus4296@swu.ac.kr>
2022-07-15 13:06:04 +0900

    [FEAT] 화면 연결 플로우 추가 (#48)

--- POME-iOS/POME-iOS/Screen/Write/Cell/CVC/SpendCVC.swift
+++ POME-iOS/POME-iOS/Screen/Write/Cell/CVC/SpendCVC.swift
@@ -13,11 +13,12 @@ class SpendCVC: BaseCVC {
     
     /// 액션 시트를 띄우기 위한 클로저 선언
     var tapMoreBtn: (() -> ())?
+    var tapPlusBtn: (() -> ())?
 
     // MARK: IBOutlet
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var leftEmojiImageView: UIImageView!
-    @IBOutlet weak var rightEmojiImageView: UIImageView!
+    @IBOutlet weak var rightEmojiBtn: UIButton!
     @IBOutlet weak var priceLabel: UILabel!
     @IBOutlet weak var contentLabel: UILabel!
     
@@ -27,7 +28,19 @@ class SpendCVC: BaseCVC {
     }
     
     // MARK: IBAction
-    @IBAction func tapMoreBtn(_ sender: Any) {
+    @IBAction func tapMoreBtn(_ sender: UIButton) {
         tapMoreBtn?()
+    }
+    
+    @IBAction func tapPlusBtn(_ sender: UIButton) {
+        tapPlusBtn?()
+    }
+}
+
+// MARK: - Custom Methods
+extension SpendCVC {
+    func setData() {
+        
+        // TODO: - 네트워크 통신할 때 코드 추가 필요
     }
 }
