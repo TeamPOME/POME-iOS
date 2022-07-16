5b0268c531970fe508cf41e72a5a186034f1916a
juhyeon <wngus4296@swu.ac.kr>
2022-07-16 18:17:00 +0900

    [ADD] 캘린더 델리게이트 추가 (#56)

---
+++ POME-iOS/POME-iOS/Global/Protocol/CalendarDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  CalendarDelegate.swift
+//  POME-iOS
+//
+//  Created by Juhyeon Byun on 2022/07/16.
+//
+
+import Foundation
+
+protocol DeliveryDateProtocol {
+    func deliveryDate(date: Date, isStartDate: Bool)
+}
