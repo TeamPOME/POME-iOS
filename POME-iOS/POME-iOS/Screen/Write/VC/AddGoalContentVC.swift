30a5d6eca150b0d2e0e651de2b7a44be4123d637
juhyeon <wngus4296@swu.ac.kr>
2022-07-17 22:13:29 +0900

    [FEAT] 키보드 처리 (#56)

--- POME-iOS/POME-iOS/Screen/Write/VC/AddGoalContentVC.swift
+++ POME-iOS/POME-iOS/Screen/Write/VC/AddGoalContentVC.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class AddGoalContentVC: BaseVC {
-
+    
     // MARK: Properties
     var startDate: Date = Date()
     var endDate: Date = Date()
@@ -21,20 +21,34 @@ class AddGoalContentVC: BaseVC {
     @IBOutlet weak var priceTextField: PomeTextField!
     @IBOutlet weak var confirmBtn: PomeBtn!
     @IBOutlet weak var openSwitch: UISwitch!
+    @IBOutlet weak var switchBackView: UIView!
     
     // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
+        setDelegate()
     }
     
     // MARK: IBAction
     @IBAction func tapCloseBtn(_ sender: UIButton) {
         navigationController?.popToRootViewController(animated: true)
     }
+    
+    @IBAction func editCategoryTextField(_ sender: UITextField) {
+        checkMaxLength(textField: categoryTextField, maxLength: 5)
+    }
+    
+    @IBAction func editPromiseTextField(_ sender: UITextField) {
+        checkMaxLength(textField: promiseTextField, maxLength: 12)
+    }
+    
+    @IBAction func tapOpenSwitch(_ sender: UISwitch) {
+        switchBackView.backgroundColor = sender.isOn ? .pomeLightPink : .grey_0
+    }
 }
 
-// MARK: - Custom Methods
+// MARK: - UI
 extension AddGoalContentVC {
     
     private func configureUI() {
@@ -48,5 +62,51 @@ extension AddGoalContentVC {
         priceTextField.configurePlaceholder(placeholder: "50,000")
         confirmBtn.setTitle("작성했어요", for: .normal)
         confirmBtn.isDisabled = true
+    }
+}
+
+// MARK: - Custom Methods
+extension AddGoalContentVC {
+    
+    private func setDelegate() {
+        categoryTextField.delegate = self
+        promiseTextField.delegate = self
+        priceTextField.delegate = self
+    }
+    
+    /// 글자수 제한
+    private func checkMaxLength(textField: UITextField!, maxLength: Int) {
+        if (textField.text?.count ?? 0 > maxLength) {
+            textField.deleteBackward()
+        }
+    }
+}
+
+// MARK: - Types
+extension AddGoalContentVC: UITextFieldDelegate {
+    
+    /// 리턴을 누르면 다음 텍스트 필드로 이동, 다음 텍스트 필드가 차있으면 키보드 내림
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == categoryTextField {
+            if promiseTextField.isEmpty {
+                promiseTextField.becomeFirstResponder()
+            } else if priceTextField.isEmpty {
+                priceTextField.becomeFirstResponder()
+            }
+        } else if textField == promiseTextField && priceTextField.isEmpty {
+            priceTextField.becomeFirstResponder()
+        }
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        /// 세 텍스트 필드 모두 작성했으면 하단 작성했어요 버튼 활성화
+        if !categoryTextField.isEmpty && !promiseTextField.isEmpty && !priceTextField.isEmpty {
+            confirmBtn.isDisabled = false
+        } else {
+            confirmBtn.isDisabled = true
+        }
     }
 }
