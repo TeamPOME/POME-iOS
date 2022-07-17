d61afe463e2ecd7df6c48252b33af1209c3cd86f
eunjuchoi <eunjuzzoo@naver.com>
2022-07-18 04:38:03 +0900

    [ADD] 카카오 로그인에 필요한 설정 (#63)

--- POME-iOS/POME-iOS/Support/AppDelegate.swift
+++ POME-iOS/POME-iOS/Support/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import KakaoSDKCommon
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        KakaoSDK.initSDK(appKey: "699dbdf059e2987fa055e4e3a50d524d")
+        
         return true
     }
 
--- POME-iOS/POME-iOS/Support/SceneDelegate.swift
+++ POME-iOS/POME-iOS/Support/SceneDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import KakaoSDKAuth
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
@@ -24,6 +25,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             
             self.window = window
             window.makeKeyAndVisible()
+        }
+    }
+    
+    /// 카카오 로그인에 필요한 설정
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        if let url = URLContexts.first?.url {
+            if (AuthApi.isKakaoTalkLoginUrl(url)) {
+                _ = AuthController.handleOpenUrl(url: url)
+            }
         }
     }
     
