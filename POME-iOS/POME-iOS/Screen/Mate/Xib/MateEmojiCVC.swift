14aa089b49c6748e397b627065470d5dc363d5fa
yujindonut <hyujin0825@gmail.com>
2022-07-19 17:59:10 +0900

    [FEAT] 바텀시트 생성 (#67)

---
+++ POME-iOS/POME-iOS/Screen/Mate/Xib/MateEmojiCVC.swift
@@ -0,0 +1,20 @@
+//
+//  MateEmojiCVC.swift
+//  POME-iOS
+//
+//  Created by 한유진 on 2022/07/19.
+//
+
+import UIKit
+
+class MateEmojiCVC: BaseCVC {
+    
+    // MARK: IBOutlet
+    @IBOutlet weak var mateEmojiImageView: PomeMaskedImageView!
+    @IBOutlet weak var mateNameLabel: UILabel!
+    
+    // MARK: Life Cycle
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
---
+++ POME-iOS/POME-iOS/Screen/Mate/Xib/MateEmojiHeaderCVC.swift
@@ -0,0 +1,15 @@
+//
+//  MateEmojiHeaderCVC.swift
+//  POME-iOS
+//
+//  Created by 한유진 on 2022/07/19.
+//
+
+import UIKit
+
+class MateEmojiHeaderCVC: BaseCVC {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
